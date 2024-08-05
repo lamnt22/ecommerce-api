eb194fe0b5f94e7fc038c14745847c1cf43d6d28
lamss22 <lamnguyenthanh226@gmail.com>
2024-08-05 16:53:34 +0700

    update config host

--- src/main/java/project/security/SecurityConfig.java
+++ src/main/java/project/security/SecurityConfig.java
@@ -22,57 +22,77 @@ import lombok.RequiredArgsConstructor;
 import project.security.filter.CustomAuthenticationFilter;
 import project.security.filter.CustomAuthorizationFilter;
 
-
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	private final UserDetailsService userDetailsService;
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager());
-		customAuthenticationFilter.setFilterProcessesUrl("/api/login");
-		http.cors().and().csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**","/api/user/**","/api/address/**","/api/order/**").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**","/api/category/**","/api/product/**","/api/comment/**","/api/address/**","/api/order/**","/api/orderHistory/**").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/uploads/**","/api/product/createProduct/**","/api/category/createCategory").hasAnyAuthority("ROLE_ADMIN");
-		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/product/editProduct/**","/api/category/editCategory/**","/api/order/**","/api/user/**").hasAnyAuthority("ROLE_ADMIN");
-		http.authorizeRequests().antMatchers(HttpMethod.DELETE,"/api/product/deleteProduct/**","/api/category/**","/api/user/**").hasAnyAuthority("ROLE_ADMIN");
-		http.authorizeRequests().anyRequest().authenticated();
-		http.addFilter(customAuthenticationFilter);
-		http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-	}
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(bCryptPasswordEncoder);
+    }
 
-//	@Bean
-//	CorsConfigurationSource corsConfigurationSource() {
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-//		return source;
-//	}
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-	    CorsConfiguration configuration = new CorsConfiguration();
-	    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:4300", "http://localhost:8888","http://10.0.2.2:8081"));
-	    configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST","OPTIONS", "DELETE"));
-	    configuration.setAllowedHeaders(Arrays.asList("authorization","content-type"));
-	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", configuration);
-	    return source;
-	}
-	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        http.cors()
+            .and()
+            .csrf()
+            .disable();
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+            .antMatchers("/api/login/**", "/api/token/refresh/**", "/api/user/**", "/api/address/**", "/api/order/**")
+            .permitAll();
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/api/user/**", "/api/category/**", "/api/product/**", "/api/comment/**",
+                "/api/address/**", "/api/order/**", "/api/orderHistory/**")
+            .permitAll();
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/api/uploads/**", "/api/product/createProduct/**",
+                "/api/category/createCategory")
+            .hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.PUT, "/api/product/editProduct/**", "/api/category/editCategory/**",
+                "/api/order/**", "/api/user/**")
+            .hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.DELETE, "/api/product/deleteProduct/**", "/api/category/**", "/api/user/**")
+            .hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests()
+            .anyRequest()
+            .authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    // @Bean
+    // CorsConfigurationSource corsConfigurationSource() {
+    // UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    // source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    // return source;
+    // }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4300",
+            "http://192.168.1.107:8889", "http://10.0.2.2:8081"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "OPTIONS", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
